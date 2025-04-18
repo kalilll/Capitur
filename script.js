8fa2f1f351af21aa0b06595f27df84cc150e9cd8
Kalil <147331287+kalilll@users.noreply.github.com>
2025-04-18 18:48:01 +0000

    commit_18.04_002

--- script.js
+++ script.js
@@ -14,7 +14,40 @@ fetch('dados.json')
     document.getElementById('titulo').textContent = ponto.titulo;
     document.getElementById('descricao').textContent = ponto.descricao;
     document.getElementById('logo').src = ponto.logo;
+    document.getElementById('introducao').textContent = ponto.introducao;
+    document.getElementById('image1').src = ponto.image1;
+    document.getElementById('image2').src = ponto.image2;
+    document.getElementById('image3').src = ponto.image3;
 
     const hero = document.getElementById('hero');
     hero.style.backgroundImage = `url(${ponto.imagem_fundo})`;
   });
+
+  let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
