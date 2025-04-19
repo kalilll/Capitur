f4339fe79ff74c575eebd1a18c5cf19c58f1bf87
Kalil <147331287+kalilll@users.noreply.github.com>
2025-04-19 16:07:55 +0000

    commit_19_04_001

--- script.js
+++ script.js
@@ -14,40 +14,35 @@ fetch('dados.json')
     document.getElementById('titulo').textContent = ponto.titulo;
     document.getElementById('descricao').textContent = ponto.descricao;
     document.getElementById('logo').src = ponto.logo;
-    document.getElementById('introducao').textContent = ponto.introducao;
+    document.getElementById('introducao1').textContent = ponto.introducao1;
+    document.getElementById('introducao2').textContent = ponto.introducao2;
     document.getElementById('image1').src = ponto.image1;
     document.getElementById('image2').src = ponto.image2;
     document.getElementById('image3').src = ponto.image3;
+    document.getElementById('text_como_chegar1').textContent =ponto.text_como_chegar1;
+    document.getElementById('text_como_chegar2').textContent =ponto.text_como_chegar2;
+    document.getElementById('text_como_chegar3').textContent =ponto.text_como_chegar3;
+    document.getElementById('image4').src = ponto.image4;
+    document.getElementById('text_onde_ficar1').textContent =ponto.text_onde_ficar1;
+    document.getElementById('text_onde_ficar2').textContent =ponto.text_onde_ficar2;
+    document.getElementById('text_onde_ficar3').textContent =ponto.text_onde_ficar3;
 
     const hero = document.getElementById('hero');
     hero.style.backgroundImage = `url(${ponto.imagem_fundo})`;
   });
 
-  let slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+  let slideIndex = 0;
+  showSlides();
+  
+  function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}
+    slides[slideIndex-1].style.display = "block";
+    setTimeout(showSlides, 3000);
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-}
+  
